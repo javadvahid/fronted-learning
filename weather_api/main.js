d37f97108aaaf8a7fe7842c85ade7f21f81b7b90
jaVad <javadvahid@hotmail.com>
2020-10-25 22:11:10 +0330

    ajax api request

--- weather_api/main.js
+++ weather_api/main.js
@@ -3,11 +3,12 @@ $(document).ready(function(){
 
         var city = $('#name').val();
         if(city != ''){
+            $('#error').html('');
             $.ajax({
                 url: "http://api.openweathermap.org/data/2.5/weather?q=" +city+ "&units=metric" + "&APPID=4bd7d1c8eb20d85732f9235d04880760",
 
                 type:"GET",
-                dataType:"jsonp",
+                dataType:"json",
                 success: function(data){
                     console.log(data);
                     var widget = show(data);
@@ -23,6 +24,10 @@ $(document).ready(function(){
 });
 
 function show(data){
-    return "<h2><strong>Weather</strong>: "+ data.weather[0].main +"</h2>"+
-           "<h2><strong>Weather</strong>: "+ data.weather[0].description +"</h2>";
-}
+    return "<h1>Current Weather of "+ data.name+", "+ data.sys.country +"</h1>"+
+           "<h2><strong>Weather</strong>: "+ data.weather[0].main +"</h2>"+
+           "<h2><strong>description</strong>: "+ data.weather[0].description +"</h2>" + 
+           "<h2><strong>temperature</strong>: "+ data.main.temp +"</h2>" +
+           "<h2><strong>pressure</strong>: "+ data.main.pressure +"</h2>"+
+           "<h2><strong>Humidity</strong>: "+ data.main.humidity  +"</h2>"
+        }
